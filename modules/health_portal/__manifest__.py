388cb229d645b63c72aa56632c70a650e25999d5
Jacob Oldfield <jacob.oldfield@willdooit.com>
2020-04-10 15:49:27 +1000

    Initial app

---
+++ modules/health_portal/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Health Portal',
+    'version': '13.0.0.0',
+    'category': 'Medical',
+    'summary': 'Health Portal - Patient Management System',
+    'website': 'https://github.com/jacob-willdooit/CoVidooers',
+    'depends': [
+        'base',
+    ],
+    'description': """
+Health Portal - Patient Management System
+=========================================
+
+    """,
+    'data': [
+        'data/ir_module_category_data.xml',
+        'security/security.xml',
+        'views/health_portal_views.xml',
+    ],
+    'qweb': [
+    ],
+    'application': True,
+}
