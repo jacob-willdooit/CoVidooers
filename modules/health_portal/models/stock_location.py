ebae9f4dffd52555f4d36ad30b2339653586741e
Jacob Oldfield <jacob.oldfield@willdooit.com>
2020-04-11 22:23:28 +1000

    Fixes

--- modules/health_portal/models/stock_location.py
+++ modules/health_portal/models/stock_location.py
@@ -9,3 +9,20 @@ class Location(models.Model):
     ward_location = fields.Boolean('Is a Ward Location?', default=False)
     room_location = fields.Boolean('Is a Room Location?', default=False)
     patient_id = fields.Many2one('health.patient', string='Patient')
+    less_half_full_room = fields.Boolean(compute='_compute_room_full', store=True, readonly=True)
+    full_room = fields.Boolean(compute='_compute_room_full', store=True, readonly=True)
+
+    @api.depends('room_location', 'child_ids', 'child_ids.patient_id')
+    def _compute_room_full(self):
+        for location in self:
+            if location.room_location and location.child_ids:
+                pass
+                if (len(location.child_ids.mapped('patient_id')) / len(location.child_ids)) <= 0.5:
+                    location.less_half_full_room = True
+                    location.full_room = False
+                elif (len(location.child_ids.mapped('patient_id')) / len(location.child_ids)) > 0.8:
+                    location.less_half_full_room = False
+                    location.full_room = True
+            else:
+                location.less_half_full_room = False
+                location.full_room = False
